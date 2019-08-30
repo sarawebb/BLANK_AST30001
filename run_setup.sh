887faead164e0b62c8e92d6fd65d100bc02d89f9
sara webb <webb.sara.a@gmail.com>
2019-08-30 11:24:13 +1000

    final blank project

---
+++ run_setup.sh
@@ -0,0 +1,7 @@
+module purge
+module load moogsilent/july2014
+module load python/3.6.4
+module load numpy/1.14.1-python-3.6.4
+module load ipython/5.5.0-python-3.6.4
+module load matplotlib/2.2.2-python-3.6.4
+module load astropy/2.0.3-python-3.6.4
